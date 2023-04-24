4d81ec33369f7d8b92283b05684a971f00b388b7
Somatitoa <102748742+Somatitoa@users.noreply.github.com>
2023-04-25 01:57:20 +0530

    Added pagination to the News component.

--- src/components/News.js
+++ src/components/News.js
@@ -6,16 +6,49 @@ export class News extends Component {
         super();
         this.state = {
             articles: [],
+            loading: false,
+            page: 1,
+            totalResults: 0,
         };
     }
 
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=fe70e90819724323b1d498150ce7bc9f";
+        let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=fe70e90819724323b1d498150ce7bc9f&page=1&pagesize=20";
         let data = await fetch(url);
         let parsedData = await data.json();
-        console.log(parsedData);
-        this.setState({ articles: parsedData.articles });
+        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
     }
+
+    handlePrevClick = async () => {
+        console.log("previous");
+        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=fe70e90819724323b1d498150ce7bc9f&page=${this.state.page - 1}&pagesize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            articles: parsedData.articles,
+            page: this.state.page - 1,
+        });
+        setTimeout(() => {
+            window.scrollTo(0, 0);
+        }, 100);
+    };
+
+    handleNextClick = async () => {
+        console.log("next");
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+        } else {
+            let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=fe70e90819724323b1d498150ce7bc9f&page=${this.state.page + 1}&pagesize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            this.setState({
+                articles: parsedData.articles,
+                page: this.state.page + 1,
+            });
+            setTimeout(() => {
+                window.scrollTo(0, 0);
+            }, 100);
+        }
+    };
 
     render() {
         return (
@@ -29,6 +62,14 @@ export class News extends Component {
                             </div>
                         );
                     })}
+                </div>
+                <div className="d-flex justify-content-between my-4">
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>
+                        &larr; Prev
+                    </button>
+                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 20)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>
+                        Next &rarr;
+                    </button>
                 </div>
             </div>
         );
