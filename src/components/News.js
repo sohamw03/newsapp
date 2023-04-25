4e9661860042245b3a44cce6a55d5710c7169310
Somatitoa <102748742+Somatitoa@users.noreply.github.com>
2023-04-26 02:56:41 +0530

    Added category selection and routing to the app. Added spinner while news are loading ; Minor CSS improvements.

--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
 
 export class News extends Component {
+    static defaultProps = {
+        country: "in",
+        pageSize: 8,
+    };
+    static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+    };
+
     constructor() {
         super();
         this.state = {
@@ -13,37 +24,31 @@ export class News extends Component {
     }
 
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=fe70e90819724323b1d498150ce7bc9f&page=1&pagesize=20";
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fe70e90819724323b1d498150ce7bc9f&page=1&pagesize=${this.props.pageSize}`;
+        this.setState({ loading: true });
         let data = await fetch(url);
         let parsedData = await data.json();
-        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
+        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false });
     }
 
     handlePrevClick = async () => {
-        console.log("previous");
-        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=fe70e90819724323b1d498150ce7bc9f&page=${this.state.page - 1}&pagesize=20`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fe70e90819724323b1d498150ce7bc9f&page=${this.state.page - 1}&pagesize=${this.props.pageSize}`;
+        this.setState({ loading: true });
         let data = await fetch(url);
         let parsedData = await data.json();
-        this.setState({
-            articles: parsedData.articles,
-            page: this.state.page - 1,
-        });
+        this.setState({ articles: parsedData.articles, page: this.state.page - 1, loading: false });
         setTimeout(() => {
             window.scrollTo(0, 0);
         }, 100);
     };
 
     handleNextClick = async () => {
-        console.log("next");
-        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
-        } else {
-            let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=fe70e90819724323b1d498150ce7bc9f&page=${this.state.page + 1}&pagesize=20`;
+        if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
+            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fe70e90819724323b1d498150ce7bc9f&page=${this.state.page + 1}&pagesize=${this.props.pageSize}`;
+            this.setState({ loading: true });
             let data = await fetch(url);
             let parsedData = await data.json();
-            this.setState({
-                articles: parsedData.articles,
-                page: this.state.page + 1,
-            });
+            this.setState({ articles: parsedData.articles, page: this.state.page + 1, loading: false });
             setTimeout(() => {
                 window.scrollTo(0, 0);
             }, 100);
@@ -53,24 +58,28 @@ export class News extends Component {
     render() {
         return (
             <div className="container my-5">
-                <h1 className="my-3">NewsMonkey top headlines</h1>
-                <div className="row">
-                    {this.state.articles.map((e) => {
-                        return (
-                            <div className="col-md-4 my-2" key={e.url}>
-                                <NewsItem title={e.title ? e.title : ""} imgUrl={e.urlToImage ? e.urlToImage : "https://w0.peakpx.com/wallpaper/328/899/HD-wallpaper-relax-077777-dark.jpg"} description={e.description ? e.description : ""} newsUrl={e.url} />
-                            </div>
-                        );
-                    })}
-                </div>
-                <div className="d-flex justify-content-between my-4">
-                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>
-                        &larr; Prev
-                    </button>
-                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 20)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>
-                        Next &rarr;
-                    </button>
-                </div>
+                <h1 className="my-5 text-center">Top headlines</h1>
+                {(this.state.loading && <Spinner />) || (
+                    <div className="tiles">
+                        <div className="row">
+                            {this.state.articles?.map((e) => {
+                                return (
+                                    <div className="col-md-4 my-2" key={e.url}>
+                                        <NewsItem title={e.title ? e.title : ""} imgUrl={e.urlToImage ? e.urlToImage : "https://w0.peakpx.com/wallpaper/328/899/HD-wallpaper-relax-077777-dark.jpg"} description={e.description ? e.description : ""} newsUrl={e.url} />
+                                    </div>
+                                );
+                            })}
+                        </div>
+                        <div className="d-flex justify-content-between my-4">
+                            <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>
+                                &larr; Prev
+                            </button>
+                            <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>
+                                Next &rarr;
+                            </button>
+                        </div>
+                    </div>
+                )}
             </div>
         );
     }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,9 +1,6 @@
-import PropTypes from "prop-types";
 import React, { Component } from "react";
 
 export class NewsItem extends Component {
-    static propTypes = {};
-
     render() {
         let { title, description, imgUrl, newsUrl } = this.props;
         return (
@@ -11,13 +8,13 @@ export class NewsItem extends Component {
                 <div className="card rounded-4 position-relative border-1">
                     <img src={imgUrl} className="card-img-top rounded-4 rounded-bottom " alt="..." style={{ height: "17rem", objectFit: "cover" }} />
                     <div className="card-body">
-                        <h5 className="card-title" style={{ height: "3rem" }}>
+                        <h5 className="card-title" style={{ height: "3.17rem", overflow: "hidden", textOverflow: "ellipsis" }}>
                             {title.slice(0, 70) + (title.length > 70 ? "..." : "")}
                         </h5>
-                        <p className="card-text" style={{ height: "2.8rem" }}>
-                            {description.slice(0, 95) + (description.length>95?"...":"")}
+                        <p className="card-text" style={{ height: "2.8rem", overflow: "hidden", textOverflow: "ellipsis" }}>
+                            {description.slice(0, 95) + (description.length > 95 ? "..." : "")}
                         </p>
-                        <a href={newsUrl} target="_blank" className="btn btn-outline-dark btn-sm rounded-3 border-secondary stretched-link">
+                        <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-outline-dark btn-sm rounded-3 border-secondary stretched-link">
                             Read more
                         </a>
                     </div>
